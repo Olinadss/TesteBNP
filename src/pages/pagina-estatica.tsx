95a46147d53115edbf6a12967ebfb33a7589ca40
Danilo dos Santos <danilo.santossouza@outlook.com>
2024-06-29 10:57:28 -0300

    feat: faz chamada através do SSR

--- src/pages/pagina-estatica.tsx
+++ src/pages/pagina-estatica.tsx
@@ -7,43 +7,17 @@
  * - A página deve ser atualizada a cada 1 minuto
  */
 
-import { useEffect, useState } from 'react';
+import styles from '@/styles/lista.module.css'
+import { ICity } from '@/types/city.d'
 
-import styles from '@/styles/lista.module.css';
-import { ICity } from '@/types/city.d';
-
-export default function Lista() {
-	const [list, setUsers] = useState<Array<ICity>>([
-		{
-			id: new Date().getTime().toString(),
-			name: 'São Paulo',
-		},
-	]);
-
-	async function getList() {
-		try {
-			const response = await fetch('/api/cities/10');
-			const data = await response.json();
-
-			if (!response.ok) throw new Error('Erro ao obter os dados');
-
-			setUsers(data);
-		} catch (error) {
-			console.error(error);
-		}
-	}
-
-	useEffect(() => {
-		getList();
-	}, []);
-
+export default function Lista(props: { list: Array<ICity> }) {
 	return (
 		<div className={styles.container}>
 			<div className={styles.content}>
 				<h2>Lista de cidades</h2>
 
 				<div data-list-container>
-					{list.map((city) => (
+					{props.list.map(city => (
 						<div data-list-item key={city.id}>
 							{city.name}
 						</div>
@@ -51,5 +25,33 @@ export default function Lista() {
 				</div>
 			</div>
 		</div>
-	);
+	)
+}
+
+export const getServerSideProps = async () => {
+	const list = await getList()
+
+	return {
+		props: {
+			list,
+		},
+	}
+}
+
+async function getList() {
+	try {
+		const response = await fetch('http://localhost:8080/api/cities/10')
+		const data = await response.json()
+
+		if (!response.ok) throw new Error('Erro ao obter os dados')
+
+		return data
+	} catch (error) {
+		return [
+			{
+				id: new Date().getTime().toString(),
+				name: 'São Paulo',
+			},
+		]
+	}
 }
