7047b5ec88b97ccd2ea82c3598b33aa9ce5fd959
Danilo dos Santos <danilo.santossouza@outlook.com>
2024-06-29 00:51:08 -0300

    feat: faz modal de confirmacao

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -4,6 +4,7 @@ type ModalProps = {
 	children: React.ReactNode
 	title: string
 	isOpen: boolean
+	isConfirm?: boolean
 	onClose?: (type: 'click' | 'esc', target: EventTarget) => void
 	onConfirm?: () => void
 	footer?: {
--- src/pages/modal-de-confirmacao.tsx
+++ src/pages/modal-de-confirmacao.tsx
@@ -8,24 +8,45 @@
  * - O conteudo deve ser dinâmico
  */
 
-import { useState } from 'react';
-import Head from 'next/head';
+import { useState } from 'react'
+import Head from 'next/head'
 
-import styles from '@/styles/modal.module.css';
-import { Modal } from '@/components/Modal';
+import styles from '@/styles/modal.module.css'
+import { Modal } from '@/components/Modal'
 
 export default function Home() {
-	const [modalIsOpen, setModalIsOpen] = useState(false);
+	const [modalIsOpen, setModalIsOpen] = useState(false)
+
+	function handleModalConfirm() {
+		setModalIsOpen(false)
+	}
+
+	function renderModalContent() {
+		return (
+			<div data-modal-content className={styles['modal-form']}>
+				<div onSubmit={() => false}>
+					<p>Tem certeza que deseja excluir o item?</p>
+				</div>
+			</div>
+		)
+	}
 
 	return (
 		<>
 			<main className={styles.container}>
-				<button type="button" onClick={() => setModalIsOpen(true)}>
+				<button type='button' onClick={() => setModalIsOpen(true)}>
 					Abrir modal de confirmação
 				</button>
 			</main>
 
-			{/* Renderizar modal de confirmação */}
+			<Modal
+				isOpen={modalIsOpen}
+				title='Confirmação'
+				onClose={() => setModalIsOpen(false)}
+				onConfirm={handleModalConfirm}
+			>
+				{renderModalContent()}
+			</Modal>
 		</>
-	);
+	)
 }
